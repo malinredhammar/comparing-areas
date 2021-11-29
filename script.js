8569c04733bce272b370c9fd8eb8591dad8b0329
Malin Redhammar <89511902+malinredhammar@users.noreply.github.com>
2021-11-29 14:14:44 +0100

    assignment 1-comparing areas

---
+++ script.js
@@ -0,0 +1,55 @@
+   // Case one
+let triangleOne = (10 * 13) / 2;
+console.log(triangleOne);
+
+let triangleTwo = (16.5 * 20.3) / 2;
+console.log(triangleTwo);
+
+console.log(triangleOne > triangleTwo);
+
+/* Triangle 1 has a area of 65cm2
+   Triangle 2 has a area of 167.475cm2 
+   
+   Triangle Two has the biggest area*/
+
+
+
+   // Case two
+let triangleThree = (16.5 * 20.3) / 2;
+console.log(triangleThree);
+
+let triangleFour = (20.3 * 16.5) / 2;
+console.log(triangleFour);
+
+console.log(triangleThree > triangleFour);
+
+/* Triangle 3 has a area of 167,475cm2
+   Triangle 4 has a area of 167,475cm2 
+   
+   Both triangles have the same area, that is why it says 
+   false in the console, triangle three is not bigger because they 
+   have the same area*/
+
+
+   
+   // Case three
+let triangleFive = (7.8 * 5.6) / 2;
+console.log(triangleFive);
+
+let triangleSix = (9.3 * 12.4) / 2;
+console.log(triangleSix)
+
+console.log(triangleFive > triangleSix);
+
+/* Triangle 5 has a area of 21,84cm2
+   Triangle 6 has a area of 57,66cm2 
+   
+   Triangle six has the biggest area*/
+
+
+   
+   /* ANSWER TO THE QUESTION IN THE ASSIGNMENT
+   
+   JavaScript need the parentheses because it's vauled higher, therefore 
+   it will be prioritized. In this specific math problem JavaScript will first 
+   calculate the parenthesis and then devied the number by two*/
